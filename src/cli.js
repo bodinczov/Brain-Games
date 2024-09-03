cfc7d639b97d782d1985379abe9e9e41da61a65f
bodinczov <boris.od.251@mail.ru>
2024-09-03 13:11:11 +0300

    3rd step

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const getName = () => {
+    var name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+}
+export default getName;
