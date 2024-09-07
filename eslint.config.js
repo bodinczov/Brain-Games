780dcfa7216d3fe5ae62f98b69e2f8d95ccf56f5
bodinczov <boris.od.251@mail.ru>
2024-09-07 19:46:33 +0300

    minor fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,26 @@
 export default [
   {
-      ignores: ['node_modules/**', 'eslint.config.js', '/project/code/eslint.config.js'],
-      languageOptions: {
-          ecmaVersion: 'latest',
-          sourceType: 'module'
-      },
-      linterOptions: {
-          reportUnusedDisableDirectives: true,
-      },
-      rules: {
-        'quotes': ['error', 'single'], 
-        'semi': ['error', 'always'],  
-        'comma-dangle': ['error', 'never'],  
-        'indent': ['error', 2], 
-        'no-multi-spaces': 'error', 
-        'eol-last': ['error', 'always'],
-        'no-var': 'error', 
-        'prefer-const': 'error', 
-        'no-unused-vars': 'error',
-        'eqeqeq': ['error', 'always'],
-        'no-implicit-globals': 'error', 
-        'curly': ['error', 'all'],
-      },
+    ignores: ['node_modules/**', '/project/code/eslint.config.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    rules: {
+      'quotes': ['error', 'single'], 
+      'semi': ['error', 'always'],  
+      'comma-dangle': ['error', 'never'],  
+      'indent': ['error', 2], 
+      'no-multi-spaces': 'error', 
+      'eol-last': ['error', 'always'],
+      'no-var': 'error', 
+      'prefer-const': 'error', 
+      'no-unused-vars': 'error',
+      'eqeqeq': ['error', 'always'],
+      'no-implicit-globals': 'error', 
+      'curly': ['error', 'all']
+    }
   }
 ];
