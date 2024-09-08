d86a44170cc176792935401f697fbf3dd2595851
bodinczov <boris.od.251@mail.ru>
2024-09-08 11:57:31 +0300

    minor fixes - by linter

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -16,7 +16,7 @@ for (let i = 0; i < 4; i += 1) {
   }
   const randInt = Math.round((Math.random() * 50) + 1); // +1 чтобы не выпал 0
   let correctAnswer = '';
-  if (randInt === 1 || randInt === 2 || randInt === 3 || randInt === 5 || randInt === 7) {
+  if (randInt === 2 || randInt === 3 || randInt === 5 || randInt === 7) {
     correctAnswer = 'yes';
   } else if (randInt % 3 === 0) {
     correctAnswer = 'no';
@@ -25,6 +25,8 @@ for (let i = 0; i < 4; i += 1) {
   } else if (randInt % 7 === 0) {
     correctAnswer = 'no';
   } else if (randInt % 2 === 0) {
+    correctAnswer = 'no';
+  } else if (randInt === 1) {
     correctAnswer = 'no';
   } else {
     correctAnswer = 'yes';
